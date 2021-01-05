313fc42c852fb4ef4f534476cdf76417c174e488
octos-network <octochicken7aber3@gmail.com>
2021-01-05 13:52:45 +0100

    First 5 Levels

---
+++ Assets/Scripts/PlayerCollition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollition : MonoBehaviour
+{
+    public PlayerMovement movement;
+   
+
+    void OnCollisionEnter (Collision collisionInfo) 
+    {
+       if (collisionInfo.collider.tag == "Obstacle") 
+       {
+           movement.enabled = false;
+           FindObjectOfType<GameManager>().EndGame();
+       }
+    }
+}
